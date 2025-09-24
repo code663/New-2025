cc87b3b3c3efa8a25ba085153d24feb4c96487a3
MRPAWANMUSIC <146386254+code663@users.noreply.github.com>
2025-09-24 10:28:25 +0530

    Update dev.py

--- AnonXMusic/plugins/tools/dev.py
+++ AnonXMusic/plugins/tools/dev.py
@@ -13,7 +13,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from AnonXMusic import app
 
 
-DEV = [544633527,5455548710]
+DEV = [544633527,6329937391]
 
 async def aexec(code, client, message):
     exec(
